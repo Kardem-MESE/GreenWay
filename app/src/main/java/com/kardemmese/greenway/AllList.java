eb4f27fe9b3d3105c924fa753316255c3ed8ca2b
Kardem-MESE <101044970+Kardem-MESE@users.noreply.github.com>
2022-03-16 12:58:42 +0300

    son hali

--- app/src/main/java/com/kardemmese/greenway/AllList.java
+++ app/src/main/java/com/kardemmese/greenway/AllList.java
@@ -13,59 +13,51 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class AllList extends AppCompatActivity implements View.OnClickListener {
-private DatabaseReference mDatabase;
-private Button btnsave;
-private Button btnProceed;
-private TextView textViewLatitude;
-private TextView textViewLongitude;
+public class AllList extends AppCompatActivity {
+
+    EditText aName;
+    EditText aLatitude;
+    EditText aLongitude;
+    Button btnsave;
 
 
+    DatabaseReference mapDbref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_list);
-        btnProceed = (Button) findViewById(R.id.btnProceed);
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
-        textViewLatitude=(TextView) findViewById(R.id.textViewLatitude);
-        textViewLongitude=(TextView) findViewById(R.id.textViewLongitude);
-        btnsave=(Button) findViewById(R.id.btnsave);
-        btnsave.setOnClickListener(this);
-        btnProceed.setOnClickListener(new View.OnClickListener() {
+
+        aName = findViewById(R.id.editTextTextPersonName5);
+        aLatitude = findViewById(R.id.editTextTextPersonName3);
+        aLongitude = findViewById(R.id.editTextTextPersonName4);
+        btnsave= findViewById(R.id.btnsave);
+
+
+        mapDbref = FirebaseDatabase.getInstance().getReference().child("Maps");
+
+        btnsave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(AllList.this,MapsActivity.class);
-                startActivity(i);
+                insertAdvertsData();
             }
         });
+
+
+
     }
-    private void saveUserInformation(){
-        double latitude = Double.parseDouble(textViewLatitude.getText().toString().trim());
-        double longitude = Double.parseDouble(textViewLongitude.getText().toString().trim());
-        Userinformation userinformation = new Userinformation(latitude,longitude);
-        mDatabase.child("Users").setValue(userinformation);
-        Toast.makeText(this,"Saved",Toast.LENGTH_LONG).show();
-    }
-    //emrahı çok seviyom emrenin aq
+    private void  insertAdvertsData(){
+        String lat = aLatitude.getText().toString();
+        String lon = aLongitude.getText().toString();
 
-    @Override
-    public void onClick(View view) {
-        if (view == btnProceed){
-            finish();
-        }
-        if (view == btnsave){
-            saveUserInformation();
+        Adverts adverts = new Adverts(lat,lon);
 
-            textViewLatitude.setText("");
-            textViewLongitude.setText("");
-
-
-        }
+        mapDbref.push().setValue(adverts);
+        Toast.makeText(AllList.this,"Data inserted!",Toast.LENGTH_SHORT).show();
 
     }
 
-    public void PROCEED(View view) {
-        Intent intent = new Intent(AllList.this,MapsActivity.class);
+    public void SAVE(View view) {
+        Intent intent = new Intent(AllList.this,NewAdvert.class);
         startActivity(intent);
     }
 }
